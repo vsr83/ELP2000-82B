33f9df735329d0799c2548a634421f371d8aac66
Ville Räisänen <vsr@vsr.name>
2023-02-02 18:12:04 +0200

    Commented the test code

--- test/test.js
+++ test/test.js
@@ -2,6 +2,15 @@ import { elp2000SphericalOfDate, elp2000CartesianOfDate, elp2000CartesianJ2000 }
 import { horizon_array_hourly_2020 } from '../data/horizons_results_hourly_2020.js';
 import { horizon_array_monthly_1900_2100 } from '../data/horizons_results_monthly_1900-2100.js';
 import { elp2000ref_array_monthly_1900_2100 } from '../data/elp2000ref_results_monthly_1900-2100.js';
+
+/**
+ * Compute the position of the Moon for a sequence of Julian times and compute
+ * errors to the given coordinates.
+ * 
+ * @param {*} array 
+ *      The expected array with rows in the format [JT, X, Y, Z].
+ * @returns Object with maximum, minimum and average errors.
+ */
 function checkArray(array)
 {
     let maxError = 0;
@@ -15,6 +24,7 @@ function checkArray(array)
         const computed = elp2000CartesianJ2000(JT);
         const expected = [values[1], values[2], values[3]];
 
+        // Difference between the two coordinate vectors.
         const diff = [
             computed[0] - expected[0], 
             computed[1] - expected[1], 
@@ -31,11 +41,8 @@ function checkArray(array)
         {
             minError = diffNorm;
         }
-
-
-        //console.log(JT + " " + diffNorm);
-
     }
+
     avgError /= horizon_array_hourly_2020.length;
 
     return {avgError : avgError, maxError : maxError, minError : minError};
@@ -44,17 +51,21 @@ function checkArray(array)
 describe('Elp2000-82b', function() {
     describe('elp2000', function() {
         it('2020-01-01', function() {
+            // Dummy example.
             console.log(elp2000CartesianJ2000(2458849.5));
         });
 
+        // Compare to the hourly data from JPL Horizons.
         it('JPL Horizons 2020 hourly', function() {
             console.log(checkArray(horizon_array_hourly_2020));
         });
 
+        // Compare to the montly data from JPL Horizons.
         it('JPL Horizons 1900-2100 monthly', function() {
             console.log(checkArray(horizon_array_monthly_1900_2100));
         });
 
+        // Compare to data from the Fortran reference implementation.
         it('ELP2000-82b Reference Implementation 1900-2100 monthly', function() {
             console.log(checkArray(elp2000ref_array_monthly_1900_2100));
         });
